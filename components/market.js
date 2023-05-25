65e14f02f6431d89930e81906634c41d09ca33e4
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2023-05-25 19:23:52 -0300

    add cancelBuyOrder method

--- components/market.js
+++ components/market.js
@@ -212,6 +212,40 @@ SteamCommunity.prototype.buyOrderStatus = function(appId, marketHashName, buyOrd
 	}, "steamcommunity");
 };
 
+SteamCommunity.prototype.cancelBuyOrder = function(buyOrderId, callback) {
+	const self = this
+	this.httpRequest({
+		uri: 'https://steamcommunity.com/market/cancelbuyorder',
+		method: 'POST',
+		form: {
+			sessionid: self.getSessionID(),
+			buy_orderid: buyOrderId
+		},
+		headers: {
+			Referer: 'https://steamcommunity.com/market',
+			Origin: 'https://steamcommunity.com',
+			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+			'X-Prototype-Version': '1.7',
+			'X-Requested-With': 'XMLHttpRequest'
+		},
+		json: true
+	},function (err, response, body) {
+		if (err) {
+			callback(err);
+			return;
+		}
+
+		if (body.success && body.success != SteamCommunity.EResult.OK) {
+			let err = new Error(body.message || SteamCommunity.EResult[body.success]);
+			err.eresult = err.code = body.success;
+			callback(err);
+			return;
+		}
+
+        callback(null, body);
+	}, "steamcommunity");
+};
+
 /**
  * Check if an item is eligible to be turned into gems and if so, get its gem value
  * @param {int} appid
